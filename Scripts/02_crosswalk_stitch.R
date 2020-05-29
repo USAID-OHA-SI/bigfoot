d14a7c19c6609c67c210b6b47c3a3f37d1110e64
Joshua77@gmail.com <joshdavis>
2020-05-29 17:48:36 -0400

    updated for 2020.05.25 new structure

--- Scripts/02_crosswalk_stitch.R
+++ Scripts/02_crosswalk_stitch.R
@@ -3,7 +3,7 @@
 ## LICENSE:  MIT
 ## PURPOSE:  stitch together crosswalk data
 ## DETAIL :  append 'crosswalk' files downloaded from https://drive.google.com/drive/u/0/folders/10CIWaSqinny3RjCfsFfMPsf_Xl7GIAUj
-##  on 4.30.20
+## update:  5.28, adjust for new file structure from PSM
 
 #Dependancies-----------------------------------------------------
 library(tidyverse)
@@ -14,7 +14,7 @@ library(fs)
 
 #Globals----------------------------------------------------------
 data_in <- "data"
-data_out <- "C:/Users/Josh/Documents/data/fy20_q1_v2/scm/crosswalk"
+data_out <- "C:/Users/Josh/Documents/data/fy20_q2_v1/psm/data"
 images <- "Images"
 
 prinf <- function(df) {
@@ -23,48 +23,37 @@ prinf <- function(df) {
 
 #read in and munge-----------------------------------------------
 
+#list files
 files <- list.files(data_in, ".xlsx", full.names = TRUE)
 
-##delete me
-file <- file.path(data_in, "Angola LMIS-DATIM.xlsx")
+#delete
 
-df_cross <- map_dfr(.x = files,
-                    .f = ~ foo(.x))
-
+#function to stitch together and fix
 foo <- function(file){
   
-  filename <- paste(file)
-  
-  df <- read_xlsx(file, col_types = c("text")) %>%
-    mutate(ou = filename)
+  df <- read_xlsx(file,
+                  sheet = "Mapped Facilities",
+                  skip = 1,
+                  col_types = c("text")) %>%
+    mutate(ou = basename(file)) %>% 
+    rename_all(~tolower(.)) %>% 
+    fill(`similarity threshold`, .direction = "updown")
   
     return(df)
 }
+
+#create df
+df_cross <- map_dfr(.x = files,
+                    .f = ~ foo(.x))
+
  
-## fix ou
+## clean up, fix ou
 
 df_cross <- df_cross %>% 
-  mutate(ou = str_remove(ou, "data/")) %>% 
-  mutate(ou = str_remove(ou, " LMIS-DATIM.xlsx"))
+  mutate(ou = str_remove(ou, " LMIS MER Mapping.xlsx")) %>% 
+  select(-`...6`, -`...7`, -`...8`, -`...9`) %>% 
+  rename(threshold = `similarity threshold`)
 
-## examine
-glimpse(df_cross)
-
-df_cross <- df_cross %>% 
-  filter_at(vars(starts_with("level")), any_vars(!is.na(.)))
-
-#look at uids
-df_cross %>% 
-  group_by(ou) %>% 
-  summarise(unqiue_uid = n_distinct(uid),
-            unqiue_cd = n_distinct(FacilityCD),
-            unique_name = n_distinct(Facility))
-
-
-
-
-
-
-
-
-
+#write
+df_cross %>%
+  write_csv(file.path(data_out, "lmis_mer_crosswalk_all.csv"))
