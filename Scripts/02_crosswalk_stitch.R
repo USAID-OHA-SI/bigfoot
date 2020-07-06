4c1a1f4e58d2e1996395271e5567f65322b39fac
Joshua77@gmail.com <joshdavis>
2020-07-06 18:32:54 -0400

    updated for new format from PSM + summaries

--- Scripts/02_crosswalk_stitch.R
+++ Scripts/02_crosswalk_stitch.R
@@ -4,6 +4,9 @@
 ## PURPOSE:  stitch together crosswalk data
 ## DETAIL :  append 'crosswalk' files downloaded from https://drive.google.com/drive/u/0/folders/10CIWaSqinny3RjCfsFfMPsf_Xl7GIAUj
 ## update:  5.28, adjust for new file structure from PSM
+##          7.6 update for new file structure from PSM
+
+
 
 #Dependancies-----------------------------------------------------
 library(tidyverse)
@@ -29,31 +32,76 @@ files <- list.files(data_in, ".xlsx", full.names = TRUE)
 #delete
 
 #function to stitch together and fix
-foo <- function(file){
+stitch_xwalk <- function(file){
   
   df <- read_xlsx(file,
-                  sheet = "Mapped Facilities",
-                  skip = 1,
+                  sheet = "Mapped Facilities v2",
                   col_types = c("text")) %>%
     mutate(ou = basename(file)) %>% 
-    rename_all(~tolower(.)) %>% 
-    fill(`similarity threshold`, .direction = "updown")
+    rename_all(~tolower(.))
   
     return(df)
 }
 
 #create df
 df_cross <- map_dfr(.x = files,
-                    .f = ~ foo(.x))
+                    .f = ~ stitch_xwalk(.x))
 
  
 ## clean up, fix ou
 
-df_cross <- df_cross %>% 
-  mutate(ou = str_remove(ou, " LMIS MER Mapping.xlsx")) %>% 
-  select(-`...6`, -`...7`, -`...8`, -`...9`) %>% 
-  rename(threshold = `similarity threshold`)
+xwalk <- df_cross %>% 
+  mutate(ou = str_remove(ou, " LMIS MER Mapping v2.xlsx")) %>% 
+  select(-`...7`) %>%
+  rename(threshold = `similarity threshold`) %>%
+  mutate(across((c("similarity", "threshold")), as.numeric(vars)))
+
+  group_by(ou) %>% 
+  fill(threshold, .direction = "updown") %>% 
+  ungroup()
+
+## examine
+
+# how many obs have a 0 value for similarity, ie they didn't match
+xwalk %>% 
+  group_by(ou) %>% 
+  tally(similarity == "0")
+
+#how many have a non-missing orgunituid, meaning they did match
+xwalk %>% 
+  group_by(ou) %>% 
+  tally(!is.na(orgunituid))
+
+
 
 #write
 df_cross %>%
   write_csv(file.path(data_out, "lmis_mer_crosswalk_all.csv"))
+
+##
+
+
+## scratch
+## function with `fill` for threshold inputation
+stitch_xwalk <- function(file){
+  
+  df <- read_xlsx(file,
+                  sheet = "Mapped Facilities v2",
+                  skip = 1,
+                  col_types = c("text")) %>%
+    mutate(ou = basename(file)) %>% 
+    rename_all(~tolower(.)) %>% 
+    fill(`similarity threshold`, .direction = "updown")
+  
+  return(df)
+}
+
+
+
+
+
+
+
+
+
+
