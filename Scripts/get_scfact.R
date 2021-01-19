e6cb4509490bc0124109117aba7584f3739c6bd3
Joshua77@gmail.com <joshdavis>
2021-01-18 20:49:52 -0500

    add param for folderpath

--- Scripts/get_scfact.R
+++ Scripts/get_scfact.R
@@ -1,11 +1,13 @@
 #' get_scfact
 #'
 #' @param filename filename of most current SC_FACT dataset
+#' https://drive.google.com/drive/u/1/folders/1og4f-ZVzIF2H3TjfxfvLlJv1HYRU_0zD
+#' @param path folderpath where you want to save
 #'
 #' @export
 #'
 #' @examples
-get_scfact <- function(filename){
+get_scfact <- function(filename, path){
   
 glamr::import_drivefile(drive_folder = "1og4f-ZVzIF2H3TjfxfvLlJv1HYRU_0zD",
                         filename = filename,
