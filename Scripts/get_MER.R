1f72121082aba13b3b02e87bd1c1057b8a292d28
Joshua77@gmail.com <joshdavis>
2021-02-05 23:25:04 -0500

    reworked to make smaller and faster
    changed structure around to make it a little more efficient to run

--- Scripts/get_MER.R
+++ Scripts/get_MER.R
@@ -1,6 +1,6 @@
 #' get_MER
 #'
-#' @param filepath 
+#' @param filepath path of mer data. Default = mer_data
 #' @param filename name of the MSD or genie export
 #'
 #' @return
@@ -8,35 +8,42 @@
 #'
 #' @examples
 
-get_mer <- function(filepath, filename) {
+get_mer <- function(filepath = mer_data, filename) {
   
   indc <- c("PrEP_CURR", "TB_PREV", "HTS_TST", "HTS_TST_POS", "SC_CURR", "SC_ARVDISP")
   
   df <- ICPIutilities::read_msd(file.path(filepath, filename))
-  df <- ICPIutilities::read_msd(file.path(filepath, filename))
-  
-  df <- df_mer %>%
-    select(sitename, orgunituid, fundingagency, operatingunit, snu1, psnu, standardizeddisaggregate,
-           otherdisaggregate, indicator, fiscal_year, targets, qtr1, qtr2, qtr3, qtr4, cumulative, trendscoarse)
-  
-  df <- df %>% 
-    ICPIutilities::reshape_msd("long") 
   
   df <- df %>%
-    filter(indicator %in% c("TX_CURR", "TX_NEW") &
-        standardizeddisaggregate %in%
-        c("Age/Sex/ARVDispense/HIVStatus", "Total Numerator", "Age/Sex/HIVStatus")) |
-        indicator %in% indc %>%
-    filter(standardizeddisaggregate != "KeyPop/HIVStatus") %>% 
-    group_by(sitename, orgunituid, operatingunit, snu1, psnu, standardizeddisaggregate,
-             otherdisaggregate, indicator, period) %>%
-    summarise(value = sum(val)) %>%
-    ungroup()
-
+    dplyr::select(sitename, orgunituid, fundingagency, operatingunit, snu1, psnu, standardizeddisaggregate,
+           otherdisaggregate, trendscoarse, indicator, fiscal_year, targets, qtr1, qtr2, qtr3, qtr4, cumulative)
+  
+  #split off txcurr
+  dftx <- df %>% 
+    dplyr::filter(indicator == "TX_CURR",
+           standardizeddisaggregate == "Age/Sex/HIVStatus") %>% 
+    dplyr::group_by(across(c(sitename:fiscal_year))) %>% 
+    dplyr::summarise(across(where(is.numeric), sum, na.rm = TRUE))
+  
+  #create df for the rest
+  dfo <- df %>% 
+    dplyr::filter((indicator == "TX_CURR" &
+              standardizeddisaggregate %in% c("Age/Sex/ARVDispense/HIVStatus", "Total Numerator")) |
+             (indicator %in% indc & standardizeddisaggregate == "Total Numerator")) %>%
+    dplyr::filter(standardizeddisaggregate != "KeyPop/HIVStatus")
+  
+  ##bind them and make long
+  dfall <- 
+    dplyr::bind_rows(dftx, dfo) %>% 
+    ICPIutilities::reshape_msd("long")
+  
   
   #Add months of treatment
-  df <- df %>% 
-    mutate(value = case_when(otherdisaggregate %in% c("ARV Bottles - TLD 90-count", "ARV Bottles - TLE/400 90-count") ~ (value*3),
-                             otherdisaggregate=="ARV Bottles - TLD 180-count" ~ (value*6),
-                             TRUE ~ value))
+  dfall <- dfall %>% 
+    dplyr::mutate(val = case_when(otherdisaggregate %in% c("ARV Bottles - TLD 90-count",
+                                                    "ARV Bottles - TLE/400 90-count") ~ (val*3),
+                           otherdisaggregate=="ARV Bottles - TLD 180-count" ~ (val*6), TRUE ~ val))
+  
+  return(dfall)
+  
 }
